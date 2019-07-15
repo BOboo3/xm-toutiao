0b0f7d6980d4813b1d1437a17dda21384540e1e7
潘瑶 <panyao0915@qq.com>
2019-07-15 15:49:16 +0800

    面包屑封装

---
+++ src/components/my-test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <p>头部</p>
+        <p class="content">
+            <slot name="con" :test="testName"></slot>
+        </p>
+        <p>底部<slot name="fot"></slot></p>
+    </div>
+    <!-- <my-test>
+      使用组件
+                <template v-slot:con="scope">内容2 {{scope.test}} </template>
+                <template slot="fot">底部2</template>
+            </my-test> -->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      testName: '组件内部数据'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,10 +3,7 @@
         <!-- 筛选容器 -->
         <el-card>
             <div slot="header">
-                <el-breadcrumb separator-class="el-icon-arrow-right">
-                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-                </el-breadcrumb>
+                <my-bread>粉丝管理</my-bread>
             </div>
         <!-- 筛选容器内容 -->
         <el-form :model="reqParams" size="small" label-width="80px">
@@ -39,12 +36,16 @@
         </el-form>
         </el-card>
         <!-- 结果容器 -->
-        <el-card></el-card>
+        <el-card>
+
+        </el-card>
     </div>
 </template>
 
 <script>
+import MyBread from '@/components/my-bread.vue'
 export default {
+  components: { MyBread },
   data () {
     return {
     // 提交给后台的筛选条件 传参
